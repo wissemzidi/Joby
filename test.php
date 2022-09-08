0b65b38530936c86b92d2e549d07aceb7301d0df
Squar3 <69780128+FatSquare@users.noreply.github.com>
2022-09-09 00:38:54 +0200

    fix curl php

--- test.php
+++ test.php
@@ -1,9 +1,13 @@
 <?php
 echo "test";
+$config['useragent'] = 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0';
+
 
 function curl_get_contents($url)
 {
   $ch = curl_init();
+  curl_setopt($ch, CURLOPT_USERAGENT, $config['useragent']);
+  curl_setopt($ch, CURLOPT_REFERER, 'https://www.google.com/');
   curl_setopt($ch, CURLOPT_URL, $url);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);

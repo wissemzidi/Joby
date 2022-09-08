4b1cee635892e14118d2fb741102d7024c8f9776
FatSquare <legitcsgo5@gmail.com>
2022-09-09 00:35:27 +0200

    test curl

--- test.php
+++ test.php
@@ -1,3 +1,19 @@
 <?php
-echo "test if php works"
+echo "test";
+
+function curl_get_contents($url)
+{
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  $data = curl_exec($ch);
+  curl_close($ch);
+  return $data;
+}
+
+
+
+echo curl_get_contents('https://www.fiverr.com/search/gigs?query=front%20end');
+
 ?>

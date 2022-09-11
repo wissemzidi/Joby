e07facda271612e3e1c05e6ccd3e128f140ed381
Squar3 <69780128+FatSquare@users.noreply.github.com>
2022-09-11 04:47:59 +0200

    debugging mysql connection

--- conn.php
+++ conn.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 $server = "localhost";
 $username = "root";
 $password = "";

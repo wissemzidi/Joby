5f9ca37ba847f9f4888035fb7dfb3365a69958df
Squar3 <69780128+FatSquare@users.noreply.github.com>
2022-09-11 04:37:33 +0200

    register/login test

---
+++ conn.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+$server = "localhost";
+$username = "root";
+$password = "";
+$db = "joby";
+$conn = new mysqli($server, $username, $password,$db) or die("ERROR #0001");
+
+class User {
+    public $id = 0;
+    public $username = "";
+    public $email = "";
+    public $perms = 0;
+
+    function isAdmin(){
+        return ($this->perms >= 3) ? true : false;
+    }
+
+
+    function set_info($_id,$_username,$_email,$_perms){
+        $this->id = $_id;
+        $this->username = $_username;
+        $this->email = $_email;
+        $this->perms = $_perms;
+    }
+
+}
+
+function create_user($uid){
+    $user = new User();
+    $user->set_info($uid,"square","squar3dev@gmail.com",0);
+    return $user;
+}
+
+$logged_in = false;
+if (isset($_SESSION['id'])){
+    $user = create_user((int)$_SESSION['id']);
+    $logged_in = true;
+}
+
+
+?>
---
+++ funcs.php
@@ -0,0 +1,51 @@
+<?php
+function verify_email($email){
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+function verify_username($user){
+    return (bool)preg_match("/^[a-zA-Z0-9_]*$/",$user);
+}
+
+function username_exists($conn,$user){
+    $query = "SELECT * FROM users WHERE username = ?;";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("s", $user);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $x = $result->num_rows > 0;
+    $stmt->close();
+    return $x;
+}
+function email_exists($conn,$mail){
+    $query = "SELECT * FROM users WHERE email = ?;";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("s", $mail);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $x = $result->num_rows > 0;
+    $stmt->close();
+
+    return $x;
+}
+function Register($conn,$name,$email,$pwd){
+    $query = "INSERT INTO users(username,email,password) VALUES(?,?,?);";
+    $stmt = $conn->prepare($query);
+    $hashed_pwd = password_hash($pwd,PASSWORD_DEFAULT);
+    $stmt->bind_param("sss", $name,$email,$hashed_pwd);
+    $stmt->execute();
+    $stmt->close();
+}
+function Login($conn,$uname,$pwd){
+    $query = "SELECT * FROM users WHERE email = ? OR username = ?;";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("ss", $uname,$uname);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = mysqli_fetch_assoc($result);
+    $hashed_pwd = ($result->num_rows === 0) ? "" : $row['password'];
+    $stmt->close();
+    return ($result->num_rows === 0 || !password_verify($pwd,$hashed_pwd)) ? false : $row['id'];
+    exit(header("Location: /"));
+}
+?>

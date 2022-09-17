9e17f39c139a1e9dbf580a91656b0fcb7146d938
Wissem-Zidi <wissem.zidi.ofc@gmail.com>
2022-09-17 16:50:17 +0100

    testing

---
+++ z.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="z.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="container">
+    <form action="" method="POST" class="ratings">
+      <label class="radio-inline">
+        <input type="radio" name="rating" value="5" onclick="submit()"/>
+        <i class="fa fa-star"></i>
+      </label>
+          <label class="radio-inline">
+        <input type="radio" name="rating" value="4" onclick="submit()"/>
+        <i class="fa fa-star"></i>
+      </label>
+      <label class="radio-inline">
+        <input type="radio" name="rating" value="3" onclick="submit()"/>
+        <i class="fa fa-star"></i>
+      </label>
+      <label class="radio-inline">
+        <input type="radio" name="rating" value="2" onclick="submit()"/>
+        <i class="fa fa-star"></i>
+      </label>
+      <label class="radio-inline">
+        <input type="radio" name="rating" value="1" onclick="submit()"/>
+        <i class="fa fa-star"></i>
+      </label>
+    </form>
+  </div>
+</body>
+</html>

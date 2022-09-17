6a771b1541f83a75b2edeacaa59cabdb7f462247
Wissem-Zidi <wissem.zidi.ofc@gmail.com>
2022-09-17 12:05:00 +0100

    ...

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="http://fonts.cdnfonts.com/css/netron" rel="stylesheet">
     <link rel="stylesheet" href="./Main page/style.css" />
     <link rel="stylesheet" href="./Main page/media.css" />
     <meta property="og:title" content="Joby">

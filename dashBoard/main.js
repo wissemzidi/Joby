1331ea485f17e112ea76c633d8e9993a0513c351
Wissem <wissem.zidi.ofc@gmail.com>
2022-10-23 15:47:40 +0100

    Delete dashBoard directory

--- dashBoard/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const fakeInput = document.getElementById("fake-input");
-const imgInput = document.getElementById("changeImg");
-fakeInput.addEventListener("click", (e) => {
-  imgInput.click();
-});
